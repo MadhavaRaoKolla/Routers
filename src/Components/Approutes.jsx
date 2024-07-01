b5007835d8a22bd6517bbc96f1b4ce079a47a806
MadhavaRaoKolla <madhavaraokolla2@gmail.com>
2024-07-01 18:09:58 +0530

    custorm form validations added

--- src/Components/Approutes.jsx
+++ src/Components/Approutes.jsx
@@ -4,11 +4,10 @@ import Home from '../Pages/Home/Home'
 import Login from '../Pages/Login/Login'
 import Register from '../Pages/Register/Register'
 import Error from '../Pages/Error/Error'
-// import { useAuth } from './Auth'
 import { AuthContext } from '../Context/Auth'
+
 const Approutes = () => {
     
-    // const {isAuth} = useAuth();
     const {isAuth} = useContext(AuthContext);
     const routes = useRoutes([
         {path:'/',element: isAuth ? <Home/> : <Navigate to="/register"/>},
--- src/Context/Auth.jsx
+++ src/Context/Auth.jsx
@@ -1,11 +1,23 @@
 import React,{createContext, useState} from 'react';
-
+import { useEffect } from 'react';
 export const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
   const [isAuth,setIsAuth] = useState(false);
-  const login = () => setIsAuth(true);
-  const logout = () => setIsAuth(false);
+  // const [user,setUser] = useState(null);
+
+  const login = (userData) => {
+    localStorage.setItem('User',JSON.stringify(userData));
+    setIsAuth(true);
+    localStorage.setItem('auth',isAuth);
+  }
+  
+  const logout = () => {
+    localStorage.removeItem('User');
+    localStorage.removeItem('auth');
+    setIsAuth(false);
+  }
+
   return ( 
     <>
       <AuthContext.Provider value={{isAuth,login,logout}}>
